26d04e024cbfe361313d7792e7b14802efc44ef3
Nidhi Rani <nidhirani@PITS-User-Nidhi.local>
2026-02-16 11:31:36 +0530

    added smtp config

--- backend/settings.py
+++ backend/settings.py
@@ -154,6 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email (SMTP)
 # Set EMAIL_BACKEND to 'django.core.mail.backends.console.EmailBackend' to print emails to console (for debugging)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
